daa225847215d3e4be067c9cb7b8a2cb7629fd42
Adi Riyanto <siyaga@users.noreply.github.com>
2020-01-22 19:48:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ptm/Proses_Berhasil.java
@@ -0,0 +1,14 @@
+package com.example.ptm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Proses_Berhasil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proses__berhasil);
+    }
+}
---
+++ app/src/main/java/com/example/ptm/Proses_Transaksi.java
@@ -0,0 +1,14 @@
+package com.example.ptm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Proses_Transaksi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proses__transaksi);
+    }
+}
